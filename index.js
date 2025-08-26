0a902ef4f157a5f70ce3d5010c2e329e52474498
KaraBrahim <brahim.kara@univ-constantine2.dz>
2025-08-26 17:05:22 +0100

    complete the second phase of animation : )

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-/* const part1 = document.getElementById("part1");
-const part2 = document.getElementById("part2");
-const part3 = document.getElementById("part3"); */
-
 const background = document.getElementById("background");
-const plain = Array.from(document.getElementsByClassName("plain"))
-console.log(plain[0]);
+const plain = Array.from(document.getElementsByClassName("plain"));
+const obstacl = document.getElementsByClassName("obstacl")[0];
 const childElements = background.children;
 const bodyWidth = document.body.clientWidth;
+let isTimeToStop = false;
+let interval;
+let plainPlace;
+let obstaclPlace;
 const parts = [];
 Array.from(childElements).forEach((e) => parts.push(e));
+
 const styleInit = (part, i) => {
   part.style.left = i * bodyWidth + "px";
 };
@@ -29,14 +30,43 @@ const moveOne = () => {
 const move = (steps = parts.length - 1) => {
   let counter = 1;
   moveOne();
-  const interval = setInterval(() => {
+  testCollision();
+  interval = setInterval(() => {
     moveOne();
     counter++;
     if (counter == steps) {
       clearInterval(interval);
     }
   }, 2900);
-  if (counter == steps) {
-    clearInterval(interval);
-  }
+};
+
+const isColliding = () => {
+  plainPlace = plain[0].getBoundingClientRect();
+  obstaclPlace = obstacl.getBoundingClientRect();
+  const margin = 110;
+  return !(plainPlace.left < obstaclPlace.left - margin);
+};
+
+const testCollision = () => {
+  const Test = setInterval(() => {
+    isTimeToStop = isColliding();
+    if (isTimeToStop) {
+      clearInterval(Test);
+      clearInterval(interval);
+      parts.forEach((part) => {
+        const computedStyle = part.getBoundingClientRect();
+        part.style.transition = "none";
+        part.style.left = computedStyle.left + "px";
+      });
+      const plainImg = plain[0].querySelector("img");
+      if (plainImg) {
+        plainImg.style.animationPlayState = "paused";
+      }
+      const boom = document.getElementsByClassName("boom");
+      Array.from(boom).forEach((boomNumber) => {
+        boomNumber.style.display = "inline";
+      });
+      console.log("game stopped!");
+    }
+  }, 50);
 };
