57e0bef4ecf64f0786530b25f1a1578fe08f6f8b
KaraBrahim <brahim.kara@univ-constantine2.dz>
2025-09-02 21:53:44 +0100

    almost the game done

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const plain = Array.from(document.getElementsByClassName("plain"));
 const obstacl = document.getElementsByClassName("obstacl")[0];
 const childElements = background.children;
 const bodyWidth = document.body.clientWidth;
+
 let isTimeToStop = false;
 let interval;
 let plainPlace;
 let obstaclPlace;
+const form = document.getElementsByTagName("form");
+const formElement = Array.from(form)[0];
 const parts = [];
-Array.from(childElements).forEach((e) => parts.push(e));
+const boomEffect = document.getElementById("boomEffect");
+const country = document.getElementById('country');
+const fname = document.getElementById('name');
+const dialogueText = Array.from(document.getElementsByClassName('dialogueText'))[0];
+const dialogueDiv = document.getElementById("dialogue");
 
+
+Array.from(childElements).forEach((e) => parts.push(e));
 const styleInit = (part, i) => {
   part.style.left = i * bodyWidth + "px";
 };
@@ -28,6 +37,8 @@ const moveOne = () => {
 };
 
 const move = (steps = parts.length - 1) => {
+  console.log(fname.value , country.value);
+  formElement.style.display = "none";
   let counter = 1;
   moveOne();
   testCollision();
@@ -62,11 +73,47 @@ const testCollision = () => {
       if (plainImg) {
         plainImg.style.animationPlayState = "paused";
       }
+      boomEffect.play();
       const boom = document.getElementsByClassName("boom");
       Array.from(boom).forEach((boomNumber) => {
         boomNumber.style.display = "inline";
       });
-      console.log("game stopped!");
+      let dialogue = `Oh my God...the terrorist ${fname.value} from ${country.value} blew up the Twin Towers. We have to put him on the blacklist!!`
+      dialogueDiv.style.display = "block"
+      typeWriter(dialogue);
     }
   }, 50);
 };
+
+const loadCountries = async () => {
+  try {
+    const response = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name"
+    );
+    let countries = await response.json();
+    countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
+    const countryElement = document.getElementById('country');
+    countries.forEach((country)=>{
+      const newCountry = document.createElement("option");
+      newCountry.textContent = country.name.common;
+      newCountry.value = country.name.common;
+      countryElement.appendChild(newCountry);
+    })
+  } catch (error) {
+    console.error(error);
+  }
+};
+loadCountries();
+
+
+const typeWriter = (message) => {
+  dialogueText.textContent = message.charAt(0);
+  let index = 1;
+const typerInterval = setInterval(()=>{
+  dialogueText.textContent += message.charAt(index);
+  index++;
+  if (index >= message.length){
+    clearInterval(typerInterval);
+  }
+}, 50);
+}
